2c1c3f894174f988cc2fb6c9f3abb3e15fea6d53
MuhammadHasnain02 <mrhasnain9009@gmail.com>
2025-10-21 20:31:09 +0500

    my-app

--- app.js
+++ app.js
@@ -4,17 +4,27 @@ let menuList = document.getElementById("menuList")
 
 // Icon Handling
 changIcon.addEventListener("click" , () => {
-
-    let icon = changIcon.children[1];
+    
+    let icon = changIcon.children[1]
 
     if (icon.classList.contains("fa-angle-down")) {
-        menuList.classList.remove("hidden")
+
+        menuList.classList.remove("hidden" , "hide-menu")
+        menuList.classList.add("show-menu");
+        menuList.style.display = "block";
+
         icon.classList.remove("fa-angle-down");
         icon.classList.add("fa-angle-up");
-    } else {
-        menuList.classList.add("hidden")
+
+    }
+    else {
+
+        menuList.classList.remove("show-menu");
+        menuList.classList.add("hidden" , "hide-menu")
+
         icon.classList.remove("fa-angle-up");
         icon.classList.add("fa-angle-down");
+
     }
 
 })
